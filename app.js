ee4f919bf6c0dbf5805f34ea91292751335b7814
Roman Hrytsyshyn <romangry.2004@gmail.com>
2022-11-22 13:33:32 +0100

    Add middlewares, router, controller and environment variables. Implement listening the server.

--- app.js
+++ app.js
@@ -1,2 +1,30 @@
 require('dotenv').config();
-console.log('Hello Node.js! :)');
+
+// imports
+const express = require('express');
+const cors = require('cors');
+
+// environment variables and server
+const PORT = process.env.PORT || 8000;
+const app = express();
+
+// routers and middlewares
+const imageRouter = require('./src/routers/image.router');
+const loggerMiddleware = require('./src/middlewares/logger.middleware');
+
+function startServer() {
+    try {
+        app.listen(PORT, () => console.log(`Server has been started on port ${PORT}`));
+    } catch (error) {
+        console.log(`Error while starting the server: ${error.message}`);
+    }
+}
+
+// middlewares
+app.use(cors({ credentials: true, origin: ['http://localhost:4200', 'http://localhost:3000'] }));
+app.use(express.static('public'));
+app.use(loggerMiddleware)
+
+app.use('/api', imageRouter)
+
+startServer();
