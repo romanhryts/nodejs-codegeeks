5ba9b0661055402e26dbb85780f3a16a2385397b
Roman Hrytsyshyn <romangry.2004@gmail.com>
2022-11-22 14:34:56 +0100

    Add deployed api link

--- src/controllers/image.controller.js
+++ src/controllers/image.controller.js
@@ -3,7 +3,7 @@ class ImageController {
     static getRandomImage(req, res) {
         try {
             const randomImageNumber = Math.floor(Math.random() * 8 + 1); // random number in range 1-8
-            const path = `/images/cats/${randomImageNumber}.jpeg`;
+            const path = `https://nodejs-api-yipg.onrender.com/images/cats/${randomImageNumber}.jpeg`;
             res.send({path})
         } catch (error) {
             res.status(500).send({message: 'Server error.'})
